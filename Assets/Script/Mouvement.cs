8ed82922966d43855d750af77bfb2804bdf71e98
Bryan Artiges <shineylogaming@gmail.com>
2021-02-02 21:10:17 +0100

    21h
    Fix setside blocked

--- Assets/Script/Mouvement.cs
+++ Assets/Script/Mouvement.cs
@@ -6,6 +6,8 @@ public class Mouvement : MonoBehaviour
 {
     private float previousTime;
     public float fallTime = 0.8f;
+    public static int height = 15;
+    public static int width = 9;
 
     // Start is called before the first frame update
     void Start()
@@ -44,11 +46,15 @@ public class Mouvement : MonoBehaviour
         {
             if (Input.GetKeyDown(KeyCode.LeftArrow))
             {
-                    transform.position += new Vector3(-1, 0, 0);               
+                transform.position += new Vector3(-1, 0, 0);
+                if (!ValidMove())
+                    transform.position -= new Vector3(-1, 0, 0);                  
             }
             else if (Input.GetKeyDown(KeyCode.RightArrow))
             {
-                    transform.position += new Vector3(1, 0, 0);
+                transform.position += new Vector3(1, 0, 0);
+                if(!ValidMove())
+                    transform.position -= new Vector3(1, 0, 0);
             }
         }
         //////////////////////
@@ -57,7 +63,24 @@ public class Mouvement : MonoBehaviour
         if (Time.time - previousTime > fallTime)
         {
             transform.position += new Vector3(0, -1, 0);
+            if (!ValidMove())
+                transform.position -= new Vector3(-1, 0, 0);
             previousTime = Time.time;
         }
+    }
+
+    bool ValidMove()
+    {
+        foreach(Transform children in transform)
+        {
+            int roundedX = Mathf.RoundToInt(children.transform.position.x);
+            int roundedY = Mathf.RoundToInt(children.transform.position.y);
+
+            if(roundedX < 0 || roundedX>= width || roundedY < 0 || roundedY>= height)
+            {
+                return false;
+            }
+        }
+        return true;
     }
 }
