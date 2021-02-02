d500e59a79ecda2650d33619fb0e6a25876fa2d1
Bryan Artiges <shineylogaming@gmail.com>
2021-02-02 14:30:10 +0100

    Ajout de 14H
    Spawner qui spawn une forme au hasard dans la liste
    Préfab d'un bloc et d'une ligne de 2 blocs

---
+++ Assets/SpawningForme.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawningForme : MonoBehaviour
+{
+    public GameObject[] Formes;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        NewForme();
+    }
+
+    // Update is called once per frame
+    public void NewForme()
+    {
+        Instantiate(Formes[Random.Range(0, Formes.Length)], transform.position, Quaternion.identity);
+    }
+}
