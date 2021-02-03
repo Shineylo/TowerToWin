26d80fbd1fe39c89f90f95d294cc073e6f8328a8
Bryan Artiges <shineylogaming@gmail.com>
2021-02-03 17:38:27 +0100

    Mercredi soir
    Rotation des pièces avec flèche haut et bas fonctionnel et destruction d'une pièce en dehors du champs de visions

--- Assets/Script/Mouvement.cs
+++ Assets/Script/Mouvement.cs
@@ -1,71 +1,85 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Mouvement : MonoBehaviour
 {
     private float previousTime;
-    public float fallTime = 0.8f;
-    public static int height = 15;
+    public float fallTime = 0.1f;
+    public static int height = 16;
     public static int width = 9;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public bool playable = true;
 
     // Update is called once per frame
     void Update()
     {
-        ///////////////////
-        ///Sur Téléphone///
-        ///////////////////
-        if (Input.touchCount > 0)
+        if (playable == true)
         {
-            var touch = Input.touches[0];
-            if (touch.position.x < Screen.width / 2)
+            ///////////////////
+            ///Sur Téléphone///
+            ///////////////////
+            if (Input.touchCount > 0)
             {
-                if (Input.GetTouch(0).phase == TouchPhase.Began)
+                var touch = Input.touches[0];
+                if (touch.position.x < Screen.width / 2)
+                {
+                    if (Input.GetTouch(0).phase == TouchPhase.Began)
+                    {
+                        transform.position += new Vector3(-1, 0, 0);
+                        if (!ValidMove())
+                            transform.position -= new Vector3(-1, 0, 0);
+                    }
+                }
+                else if (touch.position.x > Screen.width / 2)
+                {
+                    if (Input.GetTouch(0).phase == TouchPhase.Began)
+                    {
+                        transform.position += new Vector3(1, 0, 0);
+                        if (!ValidMove())
+                            transform.position -= new Vector3(1, 0, 0);
+                    }
+                }
+            }
+            else
+            ////////////////////
+            ///Sur Ordinateur///
+            ////////////////////
+            {
+                if (Input.GetKeyDown(KeyCode.LeftArrow))
                 {
                     transform.position += new Vector3(-1, 0, 0);
+                    if (!ValidMove())
+                        transform.position -= new Vector3(-1, 0, 0);
                 }
-            }
-            else if (touch.position.x > Screen.width / 2)
-            {
-                if (Input.GetTouch(0).phase == TouchPhase.Began)
+                else if (Input.GetKeyDown(KeyCode.RightArrow))
                 {
                     transform.position += new Vector3(1, 0, 0);
+                    if (!ValidMove())
+                        transform.position -= new Vector3(1, 0, 0);
+                }
+                else if (Input.GetKeyDown(KeyCode.UpArrow))
+                {
+                    transform.Rotate(0, 0, 90);
+                    if (!ValidMove())
+                        transform.Rotate(0, 0, -90);
+                }
+                else if (Input.GetKeyDown(KeyCode.DownArrow))
+                {
+                    transform.Rotate(0, 0, -90);
+                    if (!ValidMove())
+                        transform.Rotate(0, 0, 90);
                 }
             }
         }
-        else
-        ////////////////////
-        ///Sur Ordinateur///
-        ////////////////////
+        if(transform.position.y < -.5)
         {
-            if (Input.GetKeyDown(KeyCode.LeftArrow))
+            if (playable == true)
             {
-                transform.position += new Vector3(-1, 0, 0);
-                if (!ValidMove())
-                    transform.position -= new Vector3(-1, 0, 0);                  
+                FindObjectOfType<SpawningForme>().NewForme();
             }
-            else if (Input.GetKeyDown(KeyCode.RightArrow))
-            {
-                transform.position += new Vector3(1, 0, 0);
-                if(!ValidMove())
-                    transform.position -= new Vector3(1, 0, 0);
-            }
-        }
-        //////////////////////
-        ///Chute de la forme//
-        //////////////////////
-        if (Time.time - previousTime > fallTime)
-        {
-            transform.position += new Vector3(0, -1, 0);
-            if (!ValidMove())
-                transform.position -= new Vector3(-1, 0, 0);
-            previousTime = Time.time;
+            playable = false;
+            Destroy(gameObject);
         }
     }
 
@@ -83,4 +97,15 @@ public class Mouvement : MonoBehaviour
         }
         return true;
     }
+
+    void OnCollisionEnter()
+    {
+        if (playable == true)
+        {
+            FindObjectOfType<SpawningForme>().NewForme();
+        }
+        playable = false;
+
+    }
+
 }
--- Assets/Script/SpawningForme.cs
+++ Assets/Script/SpawningForme.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class SpawningForme : MonoBehaviour
 {
     public GameObject[] Formes;
-
+    public GameObject Main;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +15,10 @@ public class SpawningForme : MonoBehaviour
     // Update is called once per frame
     public void NewForme()
     {
-        Instantiate(Formes[Random.Range(0, Formes.Length)], transform.position, Quaternion.identity);
+        if (Main.GetComponent<Main>().NbrForme < 5)
+        {
+            Instantiate(Formes[Random.Range(0, Formes.Length)], transform.position, Quaternion.identity);
+            Main.GetComponent<Main>().NbrForme++;
+        }
     }
 }
